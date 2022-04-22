2c115093504956124a1bd164d87307544f1c23e7
mohammadjavad <mjkmo123@gmail.com>
2022-04-22 11:59:39 +0430

    check for game

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate dirs;
 use std::thread;
 use std::sync::mpsc;
 use std::time::Duration;
+use sysinfo::{System, SystemExt, ProcessExt};
 
 fn main() {
     // check the config file
@@ -15,8 +16,17 @@ fn main() {
 
     let observer_thread = thread::spawn(move || {
         let config = config::Config::new();
+        let mut system_info = System::new_all();
 
         loop {
+
+            system_info.refresh_all();
+
+            let game= system_info.processes_by_exact_name("");
+
+            if game.count() > 0 {
+                // todo: launch league helper;
+            }
 
             thread::sleep(
                 Duration::from_millis(
